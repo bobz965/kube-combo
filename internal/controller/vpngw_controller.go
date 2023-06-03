ffa055114a02e4107bab80df4b0ef75c4e8bf4fc
bobz965 <zhangbingbing2_yewu@cmss.chinamobile.com>
2023-06-03 17:41:29 +0800

    rbac include deployment

--- internal/controller/vpngw_controller.go
+++ internal/controller/vpngw_controller.go
@@ -347,6 +347,7 @@ func (r *VpnGwReconciler) handleAddOrUpdateVpnGw(gw *vpngwv1.VpnGw, req ctrl.Req
 //+kubebuilder:rbac:groups=vpn-gw.kube-ovn-operator.com,resources=vpngws,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=vpn-gw.kube-ovn-operator.com,resources=vpngws/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=vpn-gw.kube-ovn-operator.com,resources=vpngws/finalizers,verbs=update
+//+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
