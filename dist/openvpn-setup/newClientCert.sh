fc576d2f855ad37c1c1c2fe5bca22e8297d34ba5
bobz965 <zhangbingbing2_yewu@cmss.chinamobile.com>
2023-06-04 22:47:11 +0800

    change git ignore

---
+++ dist/openvpn-setup/newClientCert.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# generate client cert based given client key name and service ip
+# $1 should be client key name
+# $2 should be lb service external ip or floating ip
+
+EASY_RSA_LOC="/etc/openvpn/certs"
+cd $EASY_RSA_LOC
+MY_IP_ADDR="$2"
+./easyrsa build-client-full $1 nopass
+cat >${EASY_RSA_LOC}/pki/$1.ovpn <<EOF
+client
+nobind
+dev tun
+remote-cert-tls server # mitigate mitm
+remote ${MY_IP_ADDR} 1194 udp  
+# default udp 1194
+# defualt tcp 443
+redirect-gateway def1
+<key>
+`cat ${EASY_RSA_LOC}/pki/private/$1.key`
+</key>
+<cert>
+`cat ${EASY_RSA_LOC}/pki/issued/$1.crt`
+</cert>
+<ca>
+`cat ${EASY_RSA_LOC}/pki/ca.crt`
+</ca>
+EOF
+cat pki/$1.ovpn
