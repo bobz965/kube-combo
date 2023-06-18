9cc64144e013dbd094076cd07e40f819de10173a
bobz965 <jmdxjsjgcxy@gmail.com>
2023-06-18 17:51:42 +0800

    split script

---
+++ dist/strongswan-setup/03-check.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -eux
+
+domain=$(hostname --fqdn)
+if  [ "$(hostname)" = 'moon-0' ]; then
+remote_ip='10.2.0.22'
+else
+remote_ip='10.1.0.11'
+fi
+
+#
+# kick the tires.
+
+ping -n -c 4 $remote_ip || true
+swanctl --list-conns
+swanctl --list-sas
+swanctl --stats
+ip xfrm state
+ip xfrm policy
