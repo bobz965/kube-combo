5f94393fc9a695011350df7c93efb87a5089e880
bobz965 <zhangbingbing2_yewu@cmss.chinamobile.com>
2023-06-20 09:24:59 +0800

    简化 客户端 秘钥生成

--- dist/openvpn-setup/newClientCert.sh
+++ dist/openvpn-setup/newClientCert.sh
@@ -2,29 +2,29 @@
 set -euo pipefail
 # generate client cert based given client key name and service ip
 # $1 should be client key name
-# $2 should be lb service external ip or floating ip
+# PUBLIC_IP should be lb service external ip or floating ip
 
 EASY_RSA_LOC="/etc/openvpn/certs"
 cd $EASY_RSA_LOC
-MY_IP_ADDR="$2"
-./easyrsa build-client-full $1 nopass
-cat >${EASY_RSA_LOC}/pki/$1.ovpn <<EOF
+client_key_name=$1
+./easyrsa build-client-full "${client_key_name}" nopass
+cat >${EASY_RSA_LOC}/pki/"${client_key_name}" <<EOF
 client
 nobind
 dev tun
 remote-cert-tls server # mitigate mitm
-remote ${MY_IP_ADDR} 1194 udp  
+remote ${PUBLIC_IP} 1194 udp  
 # default udp 1194
 # defualt tcp 443
 redirect-gateway def1
 <key>
-`cat ${EASY_RSA_LOC}/pki/private/$1.key`
+$(cat ${EASY_RSA_LOC}/pki/private/"${client_key_name}".key)
 </key>
 <cert>
-`cat ${EASY_RSA_LOC}/pki/issued/$1.crt`
+$(cat ${EASY_RSA_LOC}/pki/issued/"${client_key_name}".crt)
 </cert>
 <ca>
-`cat ${EASY_RSA_LOC}/pki/ca.crt`
+$(cat ${EASY_RSA_LOC}/pki/ca.crt)
 </ca>
 EOF
-cat pki/$1.ovpn
+cat pki/"${client_key_name}".ovpn
